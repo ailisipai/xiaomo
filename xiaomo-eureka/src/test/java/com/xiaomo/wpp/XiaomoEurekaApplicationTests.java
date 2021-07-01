f0a1259a66f0ddbb54e7a9ae4345a02a70e1c952
ailisipai <13585951973@163.com>
2021-07-01 20:25:54 +0800

    xiaomo:eureka 服务端创建

---
+++ xiaomo-eureka/src/test/java/com/xiaomo/wpp/XiaomoEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaomo.wpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaomoEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
